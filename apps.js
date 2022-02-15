ae2da5f0e7a89155181684402466e2e6e9a0adc5
saifur550 <srstap550@gmail.com>
2022-02-15 14:28:54 +0600

    setup

---
+++ apps.js
@@ -0,0 +1,80 @@
+document.getElementById('detail-submit-btn').addEventListener ('click' , function (){
+     const buyerInput = document.getElementById('buyer-details-input');
+     console.log(buyerInput.value)
+     document.getElementById('buyer-info').innerText = buyerInput.value;
+     buyerInput.value = '';
+
+})
+
+
+
+
+document.getElementById('add-details-btn').addEventListener('click', function(){
+   
+    const itemName = document.getElementById('item-name-input').value;
+    const itemPrice = document.getElementById('item-price-input').value;
+    const itemQuantity = document.getElementById('item-quantity-input').value;
+    
+    const tableInfo = document.getElementById("info-table");
+    const totalPrice = parseFloat(itemPrice) * parseFloat(itemQuantity);
+
+
+
+    const tr  =document.createElement('tr')
+    const th  =document.createElement('th')
+    const td1 =document.createElement('td')
+    const td2 =document.createElement('td')
+    const td3 =document.createElement('td')
+
+
+    
+    td3.classList.add('item-total')
+    th.innerText = itemName;
+    td1.innerText = itemPrice;
+    td2.innerText = itemQuantity;
+    td3.innerText = totalPrice;
+
+    tr.appendChild(th)
+    tr.appendChild(td1)
+    tr.appendChild(td2)
+    tr.appendChild(td3)
+
+    tableInfo.appendChild(tr)
+    calculateSubTotal()
+
+
+})
+
+
+function calculateSubTotal(){
+    const subTotalValue = calcSubTotal();
+    const sobTotalDisplay = document.getElementById('sub-total');
+    sobTotalDisplay.innerText = subTotalValue;
+
+    const tax = subTotalValue * .2 ;
+    document.getElementById('tax').innerText = tax;
+   
+
+    
+   document.getElementById('grand-total').innerText = subTotalValue + tax ;
+
+    
+
+   
+}
+
+
+function calcSubTotal(){
+
+    let subTotal = 0;
+    const cost = document.getElementsByClassName('item-total')
+    console.log(cost);
+    for( let i = 0; i<cost.length; i++){
+        let element = cost[i];
+        const price = parseFloat(element.innerText);
+        subTotal = subTotal + price;
+    }
+    return subTotal;
+
+
+}
